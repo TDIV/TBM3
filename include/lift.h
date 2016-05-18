100bd204a4c4ff93ed734be0e121485e697fb151
Jian-Xin Zhu <jxzhu@brown.lanl.gov>
2016-05-18 14:35:44 -0600

    Initial files. JXZ

---
+++ include/lift.h
@@ -0,0 +1,11 @@
+/*
+   Created by Dr. Yuan-Yen Tai ( 2-Jul-2015 )
+   The library is used to create a tighbinding interface for different lattice structure and multi orbital freedom.
+*/
+
+#ifndef __LATTE__H__
+#define __LATTE__H__
+
+#include "../src/lat.h"
+
+#endif
