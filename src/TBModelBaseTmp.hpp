251a6addfb0bc1795a19afcbbd1f1f3a2731367f
Yuanyen Tai <anrris330@gmail.com>
2016-08-10 14:38:33 -0600

    Example for constructing program.

--- src/TBModelBaseTmp.hpp
+++ src/TBModelBaseTmp.hpp
@@ -19,6 +19,7 @@ void	TBModelBase::calculateSpinSusceptibility	(TBDataSource & rtbd){
 	
 	auto isAtom = rtbd.Lat.getAtom(vec(0,0,0));
 	
+	
 	//if( isAtom.first ){
 	//	auto allIndex = isAtom.second.allIndexList();
 	//	for( auto & orbitalIndex: allIndex){
@@ -33,23 +34,55 @@ void	TBModelBase::calculateSpinSusceptibility	(TBDataSource & rtbd){
 	//	}
 	//}
 	
-	while(rtbd.Lat.iterate()){
-		auto atom = rtbd.Lat.getAtom();
-		cout<<atom.atomName<<" "<<atom.pos<<endl;
-		for(auto & kvv: rtbd.KEigenValVec){
-			cout<<"EVal"<<endl;
-			cout<<kvv.eigenValue<<endl;
-			cout<<"EVec"<<endl;
-			cout<<kvv.eigenVector<<endl;
-			
-			cout<<endl;
-			for(unsigned ii=0 ; ii<kvv.eigenVector.cols() ; ii++){
-				for(unsigned jj=0 ; jj<kvv.eigenVector.rows() ; jj++){
-					cout<<kvv.eigenVector(ii,jj)<<" ";
-				}
-				cout<<endl;
-			}
+	double Mu = 0;
+	double eng_diff = 0.01;
+	vector<EigVec>	selectedKVV;
+	//
+	//for(auto & kvv: rtbd.KEigenValVec){
+	//	//cout<<"EVal"<<endl;
+	//	//cout<<kvv.eigenValue<<endl;
+	//	//cout<<"EVec"<<endl;
+	//	//cout<<kvv.eigenVector<<endl;
+	//	
+	//	//cout<<endl;
+	//	for(unsigned ii=0 ; ii<kvv.eigenValue.size() ; ii++){
+	//		auto Eng = kvv.eigenValue[ii];
+	//		if( abs(Eng - Mu) < eng_diff ){
+	//			selectedKVV.push_back(kvv);
+	//			break;
+	//		}
+	//	}
+	//}
+	//
+	
+	auto Nb = Lat.parameter.VEC("Nb");
+	double max_Nb = 0;
+	for(unsigned iNb = 0 ; iNb < Nb.size() ; iNb++){
+		if( Nb[iNb] > max_Nb ) max_Nb = Nb[iNb];
+	}
+	
+	map<double, pair<r_mat, double> > indexedQMap;
+	
+	for(auto & kvvI: selectedKVV)
+	for(auto & kvvJ: selectedKVV){
+		
+		auto q = kvvI.kPoint - kvvJ.kPoint;
+		
+		double dictIndex = 0;
+		double IncrementNb = 1;
+		for( unsigned i=0 ; i< Nb.size() ; i++){
+			IncrementNb *= 2*max_Nb;
+			dictIndex += IncrementNb * (q[i]+max_Nb);
 		}
+		
+		pair<r_mat, double> previouslyCalculated;
+		if( indexedQMap.find(dictIndex) != indexedQMap.end() ){
+			previouslyCalculated = indexedQMap[dictIndex];
+		}
+		
+		double valueCalculated = 0 + previouslyCalculated.second;
+		auto calculated = make_pair(q, valueCalculated);
+		indexedQMap[dictIndex] = calculated;
 		
 	}
 	
